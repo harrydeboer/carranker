183a95d7dff5972e446226292ca9130c3c6b95ae
Harry de Boer <hldeboer@gmail.com>
2019-01-21 20:28:24 +0100

    rating with earlier rating tested

--- tests/Feature/Controllers/ModelpageTest.php
+++ tests/Feature/Controllers/ModelpageTest.php
@@ -28,29 +28,46 @@ class ModelpageTest extends TestCase
         $userRepository = new UserRepository();
         $user = $userRepository->get(1);
 
-        $postArray = [
+        $postArrayFirst = [
             'generation' => $trim->getYearBegin() . '-' . $trim->getYearEnd(),
             'serie' => $trim->getFramework(),
             'trimId' => $trim->getId(),
             'content' => 'Review',
             'reCaptchaToken' => 'notused',
         ];
-
         foreach (Aspect::getAspects() as $aspect) {
-            $postArray['star'][$aspect] = '10';
+            $postArrayFirst['star'][$aspect] = '10';
         }
+
         $response = $this->actingAs($user)->post('/model/' . $trim->getModel()->getMakename() . '/' .
-            $trim->getModel()->getName(), $postArray);
-
-        $trimDB = $trimRepository->get(1);
-
+            $trim->getModel()->getName(), $postArrayFirst);
         $response->assertViewHas('isThankYou', true);
+        $response->assertStatus(200);
+
+        $trimDBFirst = $trimRepository->get(1);
 
         foreach (Aspect::getAspects() as $aspect) {
-            $rating = ($trim->getAspect($aspect) * $trim->getVotes() + $postArray['star'][$aspect]) / ($trim->getVotes() + 1);
-            $this->assertEquals($rating, $trimDB->getAspect($aspect));
+            $rating = ($trim->getAspect($aspect) * $trim->getVotes() + $postArrayFirst['star'][$aspect]) /
+                ($trim->getVotes() + 1);
+            $this->assertEquals($rating, $trimDBFirst->getAspect($aspect));
         }
 
+        $postArraySecond = $postArrayFirst;
+        foreach (Aspect::getAspects() as $aspect) {
+            $postArraySecond['star'][$aspect] = '8';
+        }
+
+        $response = $this->actingAs($user)->post('/model/' . $trim->getModel()->getMakename() . '/' .
+            $trim->getModel()->getName(), $postArraySecond);
+        $response->assertViewHas('isThankYou', true);
         $response->assertStatus(200);
+
+        $trimDBSecond = $trimRepository->get(1);
+
+        foreach (Aspect::getAspects() as $aspect) {
+            $rating = ($trimDBFirst->getAspect($aspect) * $trimDBFirst->getVotes() + $postArraySecond['star'][$aspect] -
+                    $postArrayFirst['star'][$aspect]) / $trimDBFirst->getVotes();
+            $this->assertEquals($rating, $trimDBSecond->getAspect($aspect));
+        }
     }
 }
