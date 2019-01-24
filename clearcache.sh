c2aeced179b05ed3ab66a805067ac764bb7d6173
Harry de Boer <hldeboer@gmail.com>
2019-01-24 16:53:45 +0100

    :clear instead of :cache

--- clearcache.sh
+++ clearcache.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
   php artisan cache:clear
-  php artisan route:cache
-  php artisan config:cache
+  php artisan route:clear
+  php artisan config:clear
   php artisan view:clear
-  php artisan view:cache
--- update.sh
+++ update.sh
@@ -5,10 +5,9 @@
   curl https://carranker.com/opcache_reset.php
   composer install --no-dev --no-progress --prefer-dist
   php artisan cache:clear
-  php artisan route:cache
-  php artisan config:cache
+  php artisan route:clear
+  php artisan config:clear
   php artisan view:clear
-  php artisan view:cache
   curl https://carranker.com/opcache_reset.php
   php artisan migrate --force --no-interaction
   php artisan getcmsdata
