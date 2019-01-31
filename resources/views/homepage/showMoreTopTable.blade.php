842db989ac5c39b37066623410c3f3f786489f64
Harry de Boer <hldeboer@gmail.com>
2019-01-31 17:16:32 +0100

    removed npm javascript and car is trim in showMoreTopTable blade

--- resources/views/homepage/showMoreTopTable.blade.php
+++ resources/views/homepage/showMoreTopTable.blade.php
@@ -1,14 +1,14 @@
-@foreach ($trims as $index => $car)
+@foreach ($trims as $index => $trim)
     <tr class="topRow row">
         <td class="col-md-2"><span class="numLinkTop">{{ ($offset + $index + 1) }}. </span></td>
         <td class="col-md-8">
             <div class="linkStyle">
-                <a href="{{ $car->getUrl() }}" class="linkTop">{{ $car->getFullName() }}</a>
+                <a href="{{ $trim->getUrl() }}" class="linkTop">{{ $trim->getFullName() }}</a>
             </div>
         </td>
         <td class="col-md-2">
             <span class="ratingLinkTop">
-                {{ number_format($car->getRating(), 1) }} <span class="fa fa-star"></span>
+                {{ number_format($trim->getRating(), 1) }} <span class="fa fa-star"></span>
             </span>
         </td>
     </tr>
