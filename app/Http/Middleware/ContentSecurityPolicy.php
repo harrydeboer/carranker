ec430c19426b3fb79dff05fef367a9d56e757977
Harry de Boer <hldeboer@gmail.com>
2019-01-31 11:44:36 +0100

    no npm and jquery full version instead of slim

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -16,7 +16,7 @@ class ContentSecurityPolicy
          " script-src 'self' https://www.googletagmanager.com/gtag/js https://www.google.com/recaptcha/api.js" .
         " https://www.gstatic.com https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js " .
             "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js " .
-            "https://code.jquery.com/jquery-3.3.1.slim.min.js; frame-src 'self' https://www.google.com/");
+            "https://code.jquery.com/jquery-3.3.1.min.js; frame-src 'self' https://www.google.com/");
 
         return $response;
     }
