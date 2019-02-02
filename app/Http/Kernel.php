5dd504ec256fa5e1ab482aa9bb181adef7a83436
Harry de Boer <hldeboer@gmail.com>
2019-02-02 17:47:02 +0100

    csp for every request

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\ContentSecurityPolicy::class,
     ];
 
     /**
@@ -34,7 +35,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\ContentSecurityPolicy::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
