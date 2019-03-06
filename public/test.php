0f373af8a011ae0a0e24f8a4000ec16809dc58ee
Harry de Boer <hldeboer@gmail.com>
2019-03-06 19:31:53 +0100

    tests passed

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-//simple counter to test sessions. should increment on each page reload.
-session_start();
-$count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
-
-echo $count;
-
-$_SESSION['count'] = ++$count;
--- public/testredis.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-new Redis();
-
-echo 1;
