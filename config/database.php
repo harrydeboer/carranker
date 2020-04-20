a2fe10f086d57265516258cee4962e6806fe0be2
Harry de Boer <hldeboer@gmail.com>
2020-04-20 14:20:41 +0200

    bugfix

--- config/database.php
+++ config/database.php
@@ -40,8 +40,8 @@ return [
 
 		'mysql' => [
 			'driver' => 'mysql',
-			'host' => env('DB_HOST'),
-			'port' => env('DB_PORT'),
+			'host' => env('DB_HOST', '127.0.0.1'),
+			'port' => env('DB_PORT', '3306'),
 			'database' => env('DB_DATABASE'),
 			'username' => env('DB_USERNAME'),
 			'password' => env('DB_PASSWORD'),
