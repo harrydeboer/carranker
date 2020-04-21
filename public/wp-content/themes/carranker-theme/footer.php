6269bc1c9700365b65e47e48b3caa250d19e0027
Harry de Boer <hldeboer@gmail.com>
2020-04-21 16:13:57 +0200

    menus in frontend custom theme

--- public/wp-content/themes/carranker-theme/footer.php
+++ public/wp-content/themes/carranker-theme/footer.php
@@ -2,6 +2,26 @@
 
 declare(strict_types=1);
 
+if ( has_nav_menu( 'footer' ) || ! has_nav_menu( 'expanded' ) ) {
+?>
+
+<nav class="footer-menu-wrapper" aria-label="<?php esc_attr_e( 'Horizontal', 'carranker-theme' ); ?>" role="navigation">
+
+    <ul class="footer-menu reset-list-style">
+
+		<?php
+
+		wp_nav_menu(array('theme_location' => 'footer'));
+
+        ?>
+
+    </ul> <?php
+	}
+	?>
+
+</nav><!-- .footer-menu-wrapper -->
+<?php
+
 wp_footer(); ?>
 
 </body>
--- public/wp-content/themes/carranker-theme/header.php
+++ public/wp-content/themes/carranker-theme/header.php
@@ -15,16 +15,16 @@ declare(strict_types=1);
 
 $user = wp_get_current_user();
 if ( !in_array( 'administrator', (array) $user->roles ) && !in_array( 'editor', (array) $user->roles ) ) {
-    wp_redirect('/wp-admin');
+	wp_redirect('/wp-admin');
 }
 $post = get_post();
 if ($post->post_name == 'phpinfo' && in_array( 'administrator', (array) $user->roles )) {
-    phpinfo();
-    exit;
+	phpinfo();
+	exit;
 } elseif ($post->post_name == 'opcachereset' && (APP_ENV !== 'local' && APP_ENV !== 'testing')
-    && in_array( 'administrator', (array) $user->roles )) {
-    opcache_reset();
-    echo 'Opcache reset!';
+          && in_array( 'administrator', (array) $user->roles )) {
+	opcache_reset();
+	echo 'Opcache reset!';
 }
 
 ?><!doctype html>
@@ -32,8 +32,27 @@ if ($post->post_name == 'phpinfo' && in_array( 'administrator', (array) $user->r
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
     <title><?php echo $post->post_title; ?></title>
 </head>
 
 <body>
+<?php
+if ( has_nav_menu( 'primary' ) || ! has_nav_menu( 'expanded' ) ) {
+?>
+
+<nav class="primary-menu-wrapper" aria-label="<?php esc_attr_e( 'Horizontal', 'carranker-theme' ); ?>" role="navigation">
+
+    <ul class="primary-menu reset-list-style">
+
+		<?php
+
+		wp_nav_menu(array('theme_location' => 'menu-1'));
+
+        ?>
+
+    </ul> <?php
+	}
+	?>
+
+</nav><!-- .primary-menu-wrapper -->
