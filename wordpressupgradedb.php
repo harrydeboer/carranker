1b1d59cba110cb3931f1f734b2a47991f9a4ba1e
Harry de Boer <hldeboer@gmail.com>
2020-04-21 17:44:00 +0200

    when composer installs or updates the wordpress db upgrade too

---
+++ wordpressupgradedb.php
@@ -0,0 +1,13 @@
+<?php
+
+declare( strict_types=1 );
+
+$_GET['step'] = 1;
+define( 'WP_INSTALLING', true );
+
+/** Load WordPress Bootstrap */
+require __DIR__ . '/wordpress/wp/wp-load.php';
+
+require_once("wordpress\wp\wp-admin\includes\upgrade.php");
+
+wp_upgrade();
