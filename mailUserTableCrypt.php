73125729c153dbddfea106e53669f477ed8c21ed
Harry de Boer <hldeboer@gmail.com>
2020-04-26 16:48:15 +0200

    encryptionfile documented

--- mailUserTableCrypt.php
+++ mailUserTableCrypt.php
@@ -2,6 +2,9 @@
 
 declare( strict_types=1 );
 
+/** Dovecot uses SHA-512 encryption. The passwords in the user table in the mail database need this encryption.
+ * Use single quotes around the password when running this file with php.
+ */
 if ( count( $argv ) < 2 ) {
 	echo "provide the password";
 	exit(0);
@@ -9,4 +12,4 @@ if ( count( $argv ) < 2 ) {
 
 $password = $argv[1];
 
-echo crypt( $password, '$6$' ), "\n";
+echo crypt( $password, '$6$' );
