b55dfcbe155d46a8764c9333cb95b7363d8cee9b
Harry de Boer <hldeboer@gmail.com>
2020-04-26 16:55:44 +0200

    commented wordpressupgradedb.php

--- wordpressupgradedb.php
+++ wordpressupgradedb.php
@@ -2,7 +2,11 @@
 
 declare( strict_types=1 );
 
-$_GET['step'] = 1;
+/** When wordpress upgrades it calls wp-admin/update-core.php
+ * When only the database has to upgrade it calls wp-admin/upgrade.php
+ * In that file only wp_upgrade() is needed. Wordpress and the necessary functions have to be loaded first.
+ * When the database needs no upgrade then this functions will do nothing.
+ */
 define( 'WP_INSTALLING', true );
 
 /** Load WordPress Bootstrap */
