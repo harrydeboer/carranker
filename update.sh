c32f5752bcc02c8de6f55bf30b9b28f5a01f8f4d
Harry de Boer <hldeboer@gmail.com>
2020-04-27 13:34:23 +0200

    no sudo for varnish clear

--- update.sh
+++ update.sh
@@ -13,4 +13,4 @@
   php artisan getcmsdata
   php artisan getfxrate
   php artisan flushredisdb
-  sudo varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret 'ban req.http.host ~ (^carranker.com$)'
+  varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret 'ban req.http.host ~ (^carranker.com$)'
