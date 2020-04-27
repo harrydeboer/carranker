ef9cf3022c0c7c3d1a8e9a622f5f9e70fa49486c
Harry de Boer <hldeboer@gmail.com>
2020-04-27 15:49:23 +0200

    contactpage not in varnish

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,13 @@ Route::get('auth', ['as' => 'auth', 'uses' => 'Auth\LoginController@view']);
 Route::get('register', 'Auth\RegisterController@showRegistrationForm');
 Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
 Route::get('model/{make}/{model}', 'ModelpageController@view')->where('model', '.*');
+Route::get('contact', 'ContactController@view');
 
 Route::group(['middleware' => 'cacheable'], function ()
 {
 	Route::get('search', ['as' => 'base.search', 'uses' => 'BaseController@search']);
 	Route::get('filterTop', ['as' => 'filterTop', 'uses' => 'HomepageController@filterTop']);
 	Route::get('showMoreTopTable/{numberOfRows}/{offset}', ['as' => 'showMoreTopTable', 'uses' => 'HomepageController@showMoreTopTable']);
-	Route::get('contact', 'ContactController@view');
 	Route::get('', ['as' => 'Home', 'uses' => 'HomepageController@view']);
 	Route::get('make/{make}', 'MakeController@view');
 
