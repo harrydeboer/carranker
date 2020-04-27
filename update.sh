4852051e093632d33713de079ffdf9b7dee68500
Harry de Boer <hldeboer@gmail.com>
2020-04-27 13:18:10 +0200

    restart varnish after update

--- update.sh
+++ update.sh
@@ -13,4 +13,4 @@
   php artisan getcmsdata
   php artisan getfxrate
   php artisan flushredisdb
-  sudo /etc/init.d/varnish restart
+  sudo varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret 'ban req.http.host ~ (^carranker.com$)'
