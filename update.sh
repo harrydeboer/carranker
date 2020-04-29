4362bef71c3bbc7169c38d81eb47b395d9da8565
Harry de Boer <hldeboer@gmail.com>
2020-04-29 13:15:41 +0200

    upgrade translations and root cannot do update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+  if [[ $EUID -eq 0 ]]; then
+    echo "This script must NOT be run as root" 1>&2
+    exit 1
+  fi
   prev=$(git rev-parse HEAD)
   git pull origin master
   test "$prev" == "$(git rev-parse HEAD)" && exit 1
---
+++ wpupgradedbandtranslations.php
@@ -0,0 +1,36 @@
+<?php
+
+declare( strict_types=1 );
+
+/** When wordpress upgrades it calls wp-admin/update-core.php
+ * When only the database has to upgrade it calls wp-admin/upgrade.php
+ * In that file only wp_upgrade() is needed. Wordpress and the necessary functions have to be loaded first.
+ * When the database needs no upgrade then this functions will do nothing.
+ */
+define('WP_INSTALLING', true);
+
+/** Load WordPress Bootstrap */
+require __DIR__ . "/wordpress/wp/wp-load.php";
+
+require_once(__DIR__ . "/wordpress/wp/wp-admin/includes/upgrade.php");
+
+wp_upgrade();
+
+if (get_locale() !== 'en_US') {
+	require_once __DIR__ . '/wordpress/wp/wp-admin/includes/class-wp-upgrader.php';
+
+	$url     = 'update-core.php?action=do-translation-upgrade';
+	$nonce   = 'upgrade-translations';
+	$title   = __( 'Update Translations' );
+	$context = WP_LANG_DIR;
+
+	$upgrader = new Language_Pack_Upgrader( new Language_Pack_Upgrader_Skin( [
+		'url'                => $url,
+		'nonce'              => $nonce,
+		'title'              => $title,
+		'context'            => $context,
+		'skip_header_footer' => true,
+	] ) );
+
+	$upgrader->bulk_upgrade();
+}
