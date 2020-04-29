4579fff49cac29b703691f899d10749031fb1a42
Harry de Boer <hldeboer@gmail.com>
2020-04-29 14:38:04 +0200

    random temporal name opcachereset php file

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@
   prev=$(git rev-parse HEAD)
   git pull origin master
   test "$prev" == "$(git rev-parse HEAD)" && exit 1
-  curl https://carranker.com/opcache_reset.php
+  ./opcache_reset.sh
   composer install --no-dev --no-progress --prefer-dist
   php artisan cache:clear
   php artisan route:clear
   php artisan config:clear
   php artisan view:clear
-  curl https://carranker.com/opcache_reset.php
+  ./opcache_reset.sh
   php artisan migrate --force --no-interaction
   php artisan getcmsdata
   php artisan getfxrate
