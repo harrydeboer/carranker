d7695cd43031ae323838bc8a9beebef56e7f6384
Harry de Boer <hldeboer@gmail.com>
2020-04-29 15:40:22 +0200

    removed predis and random filename opcache has 100 characters

--- opcache_reset.sh
+++ opcache_reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WEBDIR=/var/www/carranker.com/public/
-RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
+RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 100)
 echo "<?php opcache_reset(); ?>" > ${WEBDIR}${RANDOM_NAME}.php
 curl https://carranker.com/${RANDOM_NAME}.php
 rm ${WEBDIR}${RANDOM_NAME}.php
