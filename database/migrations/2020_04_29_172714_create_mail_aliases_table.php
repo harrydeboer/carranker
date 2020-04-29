02718268e505552f81c5665b716c677dc5df5097
Harry de Boer <hldeboer@gmail.com>
2020-04-29 19:32:03 +0200

    migrations mail tables

---
+++ database/migrations/2020_04_29_172714_create_mail_aliases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailAliasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail_aliases', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->integer('mail_domain_id')->unsigned();
+            $table->string('source');
+            $table->string('destination');
+
+	        $table->foreign('mail_domain_id')->references('id')->on('mail_domains');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail_aliases');
+    }
+}
