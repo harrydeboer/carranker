4625de775ba54e96a63d717b3ece5f5f3f6874a3
Harry de Boer <hldeboer@gmail.com>
2020-04-29 14:58:51 +0200

    opcache message added

--- opcache_reset.sh
+++ opcache_reset.sh
@@ -5,3 +5,5 @@ RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
 echo "<?php opcache_reset(); ?>" > ${WEBDIR}${RANDOM_NAME}.php
 curl https://carranker.com/${RANDOM_NAME}.php
 rm ${WEBDIR}${RANDOM_NAME}.php
+
+echo "Opcache has been reset!"
