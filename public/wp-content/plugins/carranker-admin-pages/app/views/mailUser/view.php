7de380101cf38614cd82fc4ba027c0a53322207f
Harry de Boer <hldeboer@gmail.com>
2020-04-30 15:59:05 +0200

    required fields in view of mail users

--- public/wp-content/plugins/carranker-admin-pages/app/views/mailUser/view.php
+++ public/wp-content/plugins/carranker-admin-pages/app/views/mailUser/view.php
@@ -3,10 +3,10 @@
 <table>
     <tr>
         <td>
-            New mail user:<form method="post" id="createMailUserForm">
-                <input type="text" name="domain" placeholder="domain">
-                <input type="password" name="password" placeholder="password">
-                <input type="email" name="email" placeholder="email">
+            Make a new mail user:<form method="post" id="createMailUserForm">
+                <input type="text" name="domain" placeholder="domain" value="carranker.com" required>
+                <input type="password" name="password" placeholder="password" required>
+                <input type="email" name="email" placeholder="email" required>
                 <input type="email" name="forward" placeholder="forward">
                 <input type="hidden" value="create" name="carrankerAdminAction">
                 <input type="submit" value="Create">
@@ -14,13 +14,17 @@
         </td>
         <td></td>
     </tr>
-    <tr><td>All mail users:</td><td></td></tr>
+	<?php if (!empty($mailUsers)) { ?>
+        <tr><td>All mail users:</td><td></td></tr>
+	<?php } else { ?>
+		<tr><td>No mail users have been created so far.</td><td></td></tr>
+    <?php } ?>
 	<?php foreach ($mailUsers as $mailUser): ?>
         <tr>
             <td>
                 <form method="post" class="updateMailUserForm">
-                    <input type="text" name="domain" value="<?= $mailUser->getDomain() ?>" placeholder="domain">
-                    <input type="email" name="email" value="<?= $mailUser->getEmail() ?>" placeholder="email">
+                    <input type="text" name="domain" value="<?= $mailUser->getDomain() ?>" placeholder="domain" required>
+                    <input type="email" name="email" value="<?= $mailUser->getEmail() ?>" placeholder="email" required>
                     <input type="email" name="forward" value="<?= $mailUser->getForward() ?>" placeholder="forward">
                     <input type="hidden" value="update" name="carrankerAdminAction">
                     <input type="hidden" value="<?= $mailUser->getId() ?>" name="id">
@@ -28,7 +32,7 @@
                 </form></td><td>
                 <form method="post">
                     Reset password
-                    <input type="password" name="password">
+                    <input type="password" name="password" required>
                     <input type="hidden" name="id" value="<?= $mailUser->getId() ?>">
                     <input type="hidden" value="updatePassword" name="carrankerAdminAction">
                     <input type="submit" value="Update">
@@ -37,14 +41,17 @@
         </tr>
 	<?php endforeach; ?>
 </table>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<form method="post" id="deleteMailUserForm">
-    <input type="email" name="deleteMailUserEmail">
-    <input type="hidden" value="delete" name="carrankerAdminAction">
-    <input type="submit" value="Delete">
-</form>
+<?php if (!empty($mailUsers)) { ?>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    Delete a mail user by email.
+    <form method="post" id="deleteMailUserForm">
+        <input type="email" name="deleteMailUserEmail" required>
+        <input type="hidden" value="delete" name="carrankerAdminAction">
+        <input type="submit" value="Delete">
+    </form>
+<?php } ?>
