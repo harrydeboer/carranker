47f7b23935cd9a0698bea78e29130165adcdcb6a
Harry de Boer <hldeboer@gmail.com>
2020-05-01 20:17:35 +0200

    bugfix

--- opcache_reset.sh
+++ opcache_reset.sh
@@ -12,7 +12,12 @@ rm ${WEBDIR}${RANDOM_NAME}.php
 WEBDIRWP=${parent_path}/wordpress/wp/
 RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 100)
 echo "<?php opcache_reset(); ?>" > ${WEBDIRWP}${RANDOM_NAME}.php
-curl https://${parentname}/${RANDOM_NAME}.php
+if [[ $parentname = "accept.carranker.com" ]]; then
+  curl https://accept.cms.carranker.com/${RANDOM_NAME}.php
+else
+  curl https://cms.carranker.com/${RANDOM_NAME}.php
+fi
+
 rm ${WEBDIRWP}${RANDOM_NAME}.php
 
 echo "Opcache has been reset!"
