6e2e17909e560e35bea5b12df0cefdb6311cf430
Harry de Boer <hldeboer@gmail.com>
2020-05-18 21:13:33 +0200

    fixed versions in composer

--- clearcache.sh
+++ clearcache.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-  php artisan cache:clear
-  php artisan route:clear
-  php artisan config:clear
-  php artisan view:clear
+if [[ ${OSTYPE} == 'msys' ]]; then
+  winpty docker exec -it carranker php artisan cache:clear
+  winpty docker exec -it carranker php artisan route:clear
+  winpty docker exec -it carranker php artisan config:clear
+  winpty docker exec -it carranker php artisan view:clear
+else
+  docker exec -it carranker php artisan cache:clear
+  docker exec -it carranker php artisan route:clear
+  docker exec -it carranker php artisan config:clear
+  docker exec -it carranker php artisan view:clear
+fi
