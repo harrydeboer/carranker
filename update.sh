e99d36c461c4fc59a79b79b43d7b3b43f2fc95cc
Harry de Boer <hldeboer@gmail.com>
2020-05-25 13:01:35 +0200

    forbidden files before rewrites

--- update.sh
+++ update.sh
@@ -6,16 +6,15 @@
   prev=$(git rev-parse HEAD)
   git pull origin master
   test "$prev" == "$(git rev-parse HEAD)" && exit 1
-  ./opcache_reset.sh
   composer install --no-dev --no-progress --prefer-dist
   php artisan cache:clear
   php artisan route:clear
   php artisan config:clear
   php artisan view:clear
-  ./opcache_reset.sh
   php artisan migrate --force --no-interaction
   php artisan getcmsdata
   php artisan getfxrate
   php artisan flushredisdb
+  ./opcache_reset.sh
   varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret 'ban req.http.host ~ (^carranker.com$)'
   echo "Varnish cache cleared!"
