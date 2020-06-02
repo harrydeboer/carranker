1b1b11d541da30b16392b9e48fd6c06fd1196723
Harry de Boer <hldeboer@gmail.com>
2020-06-02 17:54:13 +0200

    no id attribute setting necessary

--- app/Models/Elastic/Make.php
+++ app/Models/Elastic/Make.php
@@ -14,14 +14,6 @@ class Make extends BaseModel
     public $keywords = ['name', 'wiki_car_make'];
     public $texts = ['content'];
 
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-        if (isset($attributes['id'])) {
-            $this->setAttribute('id', $attributes['id']);
-        }
-    }
-
     public function getModels(): Collection
     {
         return $this->hasMany('\App\Models\Elastic\Model', 'make_id');
