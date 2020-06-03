ca6650194c04da7317b1ebe1f4971badaee00dd7
Harry de Boer <hldeboer@gmail.com>
2020-06-03 14:03:36 +0200

    flush redis test dbs in feature tests

--- app/Console/Commands/FlushRedisDB.php
+++ app/Console/Commands/FlushRedisDB.php
@@ -33,9 +33,20 @@ class FlushRedisDB extends Command
 		$this->redis = new \Redis();
 		$this->redis->connect(env('REDIS_HOST'), (int) env('REDIS_PORT'));
 		$this->redis->auth(env('REDIS_PASSWORD'));
-		$this->redis->select((int) env('REDIS_DB'));
-		$this->redis->flushDB();
+		if (env('APP_ENV') === 'testing') {
+            $this->redis->select((int) env('TEST_REDIS_DB_SESSION'));
+            $this->redis->flushDB();
+            $this->redis->select((int) env('TEST_REDIS_DB_CACHE'));
+            $this->redis->flushDB();
+            $this->redis->select((int) env('TEST_REDIS_DB'));
+            $this->redis->flushDB();
 
-		$this->info('Redis db flushed!');
+            $this->info('Redis dbs flushed!');
+        } else {
+            $this->redis->select((int)env('REDIS_DB'));
+            $this->redis->flushDB();
+            
+            $this->info('Redis db flushed!');
+        }
 	}
 }
