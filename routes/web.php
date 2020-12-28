29bbeb15e6fe3405f9bb6f33b1b6299ec5d58ec0
Harry de Boer <hldeboer@gmail.com>
2020-12-28 13:17:39 +0100

    auth routes added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('contact', ['as' => 'contact.view', 'uses' => 'ContactController@view']);
@@ -24,6 +25,7 @@ Route::get('/reset-password/{token}', ['as' => 'reset-password', 'uses' => 'Auth
     ->middleware('guest');
 Route::post('/reset-password', ['as' => 'password-update', 'uses' => 'Auth\ResetPasswordController@update'])
     ->middleware('guest');
+Auth::routes(['verify' => true]);
 
 Route::get('home', function ()
 {
