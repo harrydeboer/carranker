7c233790a704d52702d014801ed381de166c3d1e
Harry de Boer <hldeboer@gmail.com>
2020-12-31 12:36:32 +0100

    csp update for google font css

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -18,8 +18,9 @@ class ContentSecurityPolicy
          * Testing has to be done with this security enabled also.
          */
         if (env('APP_ENV') !== 'local') {
-            $response->headers->set('Content-Security-Policy', "default-src 'self'; style-src 'self';" .
-                " font-src 'self' https://fonts.googleapis.com/css2?family=Open+Sans&display=swap; " .
+            $response->headers->set('Content-Security-Policy', "default-src 'self'; style-src 'self'" .
+                " https://fonts.googleapis.com/css2?family=Open+Sans&display=swap;" .
+                " font-src 'self' https://fonts.gstatic.com/s/opensans/v18 " .
                 "img-src 'self' data:;" .
                 " script-src 'self' https://www.googletagmanager.com/gtag/js https://www.google.com/recaptcha/api.js" .
                 " https://www.gstatic.com; frame-src 'self' https://www.google.com/");
