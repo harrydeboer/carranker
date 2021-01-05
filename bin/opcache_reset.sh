e54dd279923a66a1a27b2df05a3bc5aef7863147
Harry de Boer <hldeboer@gmail.com>
2021-01-05 13:10:17 +0100

    no wordpress cms reset opcache

--- bin/opcache_reset.sh
+++ bin/opcache_reset.sh
@@ -17,18 +17,4 @@ then
 fi
 rm ${WEBDIR}${RANDOM_NAME}.php
 
-WEBDIRWP=${parent_path}/wordpress/wp/
-RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 100)
-echo "<?php opcache_reset(); ?>" > ${WEBDIRWP}${RANDOM_NAME}.php
-if [[ $parentname = "accept.carranker.com" ]]; then
-  curl https://accept.cms.carranker.com/${RANDOM_NAME}.php
-elif [[ $parentname = "carranker.com" ]]
-then
-  curl https://cms.carranker.com/${RANDOM_NAME}.php
-elif [[ $parentname = "carranker" ]]
-then
-  curl http://cms.carranker:8080/${RANDOM_NAME}.php
-fi
-rm ${WEBDIRWP}${RANDOM_NAME}.php
-
 echo "Opcache has been reset!"
