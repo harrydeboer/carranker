156ce4e86df3742d83f67eadd30e6435fbb95419
Harry de Boer <hldeboer@gmail.com>
2021-01-16 12:28:14 +0100

    delete and create indices before and after test suite

--- bin/featureTests.sh
+++ bin/featureTests.sh
@@ -2,10 +2,14 @@
 cd ..
 if [[ ${OSTYPE} == 'msys' ]]; then
   winpty docker exec -it carranker php artisan migrate --database='mysql_testing'
+  winpty docker exec -it carranker php artisan index:cars --testing
   winpty docker exec -it carranker ./vendor/bin/phpunit --configuration phpunitfeature.xml
   winpty docker exec -it carranker php artisan db:wipe --database='mysql_testing'
+  winpty docker exec -it carranker php artisan index:cars --testing
 else
   docker exec -it carranker php artisan migrate --database='mysql_testing'
+  docker exec -it carranker php artisan index:cars --testing
   docker exec -it carranker ./vendor/bin/phpunit --configuration phpunitfeature.xml
   docker exec -it carranker php artisan db:wipe --database='mysql_testing'
+  docker exec -it carranker php artisan index:cars --testing
 fi
