42a7e0b68e427ae2bc436fb0cc5b2fc95c81183c
Harry de Boer <hldeboer@gmail.com>
2021-01-17 20:35:14 +0100

    phpunit has a Unit.xml and a Feature.xml

--- scripts/unitTests.sh
+++ scripts/unitTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 if [[ ${OSTYPE} == 'msys' ]]; then
-  winpty docker exec -it carranker ./vendor/bin/phpunit --configuration phpunit.xml
+  winpty docker exec -it carranker ./vendor/bin/phpunit --configuration phpunitUnit.xml
 else
   docker exec -it carranker ./vendor/bin/phpunit --configuration phpunit.xml
 fi
