6a72eb1e1dc9e35cc0d812e81090c4ef1fe0f7a4
Harry de Boer <hldeboer@gmail.com>
2021-01-18 12:09:40 +0100

    .gitattributes export ignore tests, sh and .git files

--- unitTests.sh
+++ unitTests.sh
@@ -2,5 +2,5 @@
   if [[ ${OSTYPE} == 'msys' ]]; then
     winpty docker exec -it carranker ./vendor/bin/phpunit --configuration phpunitUnit.xml
   else
-    docker exec -it carranker ./vendor/bin/phpunit --configuration phpunit.xml
+    docker exec -it carranker ./vendor/bin/phpunit --configuration phpunitUnit.xml
   fi
