88680e4f42b28da8ffc6b572f36cfa81bb2df2bb
Harry de Boer <hldeboer@gmail.com>
2021-01-19 14:34:43 +0100

    menus and pages sql update

--- database/sql-files/menus.sql
+++ database/sql-files/menus.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
--- Host: localhost    Database: carranker
+-- Host: 127.0.0.1    Database: carranker
 -- ------------------------------------------------------
--- Server version	8.0.22-0ubuntu0.20.04.3
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -21,7 +21,7 @@
 
 LOCK TABLES `menus` WRITE;
 /*!40000 ALTER TABLE `menus` DISABLE KEYS */;
-INSERT INTO `menus` VALUES (2,'navigationFooter'),(19,'navigationHeader');
+INSERT INTO `menus` VALUES (2,'menuFooter'),(19,'menuHeader');
 /*!40000 ALTER TABLE `menus` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -34,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-05 11:39:04
+-- Dump completed on 2021-01-19 14:33:56
--- database/sql-files/pages.sql
+++ database/sql-files/pages.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
--- Host: localhost    Database: carranker
+-- Host: 127.0.0.1    Database: carranker
 -- ------------------------------------------------------
--- Server version	8.0.22-0ubuntu0.20.04.3
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -21,7 +21,7 @@
 
 LOCK TABLES `pages` WRITE;
 /*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-INSERT INTO `pages` VALUES (1,'register','','Register'),(2,'auth','','Auth'),(3,'contact','\n<p>If you have a question or a suggestion send us a mail.</p>\n','Contact'),(4,'about','\n<p>The prices on this site are in dollars.</p>\n','About'),(6,'home','<h3 id=\"rankingTheCars\">Ranking the cars</h3>\n<p>On this site you can rank any car.</p>\n<p>You can view the top of all ranked cars. This top can be filtered to only see hatchbacks for instance.</p>\n<p>What will your next car be?</p>\n','Home');
+INSERT INTO `pages` VALUES (1,'register','','Register'),(2,'auth','','Auth'),(3,'contact','\n<p>If you have a question or a suggestion send us a mail.</p>\n','Contact'),(4,'about','\n<p>The prices on this site are in dollars.</p>\n','About'),(6,'home','<h3 id=\"ranking-the-cars\">Ranking the cars</h3>\r <p>On this site you can rank any car.</p>\r <p>You can view the top of all ranked cars. This top can be filtered to only see hatchbacks for instance.</p>\r <p>What will your next car be?</p>\r ','Home');
 /*!40000 ALTER TABLE `pages` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -34,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-05 11:38:42
+-- Dump completed on 2021-01-19 14:33:13
