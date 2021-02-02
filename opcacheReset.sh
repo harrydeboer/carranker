f21412092de1cb4f658cf73a7629a795865183d8
Harry de Boer <hldeboer@gmail.com>
2021-02-02 13:52:18 +0100

    opcache reset more tidy

--- opcacheReset.sh
+++ opcacheReset.sh
@@ -6,8 +6,6 @@ RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 100).php
 echo "<?php opcache_reset(); echo 'OPcache reset!'?>" > "${PUBLIC_DIR}""${RANDOM_NAME}"
 docker cp "${PUBLIC_DIR}""${RANDOM_NAME}" carranker:/var/www/html/public/"${RANDOM_NAME}"
 
-sleep 1
-
 if [[ $PARENT_DIR = "accept.carranker.com" ]]; then
   curl https://accept.carranker.com/"${RANDOM_NAME}"
 elif [[ $PARENT_DIR = "carranker.com" ]]; then
@@ -17,5 +15,3 @@ elif [[ $PARENT_DIR = "carranker" ]]; then
 fi
 rm "${PUBLIC_DIR}""${RANDOM_NAME}"
 docker exec -it carranker rm /var/www/html/public/"${RANDOM_NAME}"
-
-echo "OPcache has been reset!"
