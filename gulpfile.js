be7f07109f7b66104214797d170b1a169e62e13c
Harry de Boer <hldeboer@gmail.com>
2021-02-17 12:41:25 +0100

    gulp watches scss files also

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp =require('gulp');
 var babel =require('gulp-babel');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 function scripts() {
   return (
@@ -13,6 +14,15 @@ function scripts() {
   );
 }
 
+function scss() {
+  return (
+      gulp.src('public/scss/*.scss')
+          .pipe(sass({outputStyle: 'compressed'}))
+          .pipe(gulp.dest('public/css'))
+  );
+}
+
 gulp.task('watch', () => {
   gulp.watch('public/src/*.js', scripts);
+  gulp.watch('public/scss/*.scss', scss);
 });
