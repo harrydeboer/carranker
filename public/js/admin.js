50d48a5f8ec2fb312a276431193f5926b1553bbd
Harry de Boer <hldeboer@gmail.com>
2021-02-17 14:49:39 +0100

    src folder for js es6

---
+++ public/js/admin.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(".delete-button").on("click",function(){$("#delete-id").val($(this).data("id")),$("#delete-form").attr("action",$(this).data("action")),$("#really-delete").show()})});
