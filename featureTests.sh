0b224b1377161a64059aaa9424a890d50d500db3
Harry de Boer <hldeboer@gmail.com>
2021-04-18 17:05:34 +0200

    refresh to fresh in migration of featureTests file

--- featureTests.sh
+++ featureTests.sh
@@ -4,7 +4,7 @@ if [[ ${OSTYPE} == 'msys' ]]; then
 else
   PREFIX=""
 fi
-$PREFIX docker exec -it carranker php artisan migrate:refresh --database='mysql_testing'
+$PREFIX docker exec -it carranker php artisan migrate:fresh --database='mysql_testing'
 $PREFIX docker exec -it carranker php artisan index:cars --testing
 $PREFIX docker exec -it carranker php artisan flush:redis-dbs --testing
 $PREFIX docker exec -it carranker ./vendor/bin/phpunit --configuration phpunitFeature.xml
